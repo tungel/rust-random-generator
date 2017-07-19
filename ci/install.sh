b07f70cb5211b8adc70255f9c9543fc7b278fe9e
tungel <everbot@live.com>
2017-07-19 11:49:26 +0930

    Install rustup and add musl target

---
+++ ci/install.sh
@@ -0,0 +1,8 @@
+# installing rustup
+# curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
+curl https://sh.rustup.rs -sSf | sh
+rustc -V
+cargo -V
+
+echo "Adding x86_64-unknown-linux-musl target..."
+rustup target add $TARGET
